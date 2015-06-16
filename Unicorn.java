78da89686a7f4063b88a78b8a15842c730cc1932
MonicaKing <msmonicaking@gmail.com>
2015-06-16 10:08:51 -0700

    Added fallingAction to the Timer in Unicorn, making it fall.

--- Unicorn.java
+++ Unicorn.java
@@ -1,58 +1,71 @@
+//The Unicorn object is also a keylistener, responding to the up key.
+
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color.*;
 
-public class Unicorn extends Drawable implements KeyListener{
-
+public class Unicorn extends Drawable implements KeyListener {
    JLabel labelForUser;
-   
-   public Unicorn(Graphics pen, Point place, ImageIcon img){
-      super(pen, place);
+   FallingAction fall;
+   Timer time;
+      
+   //Constructs a Unicorn, passing the Frame's Graphics, the point it is at originally,
+   //and the imageicon that is to be displayed representing the unicorn.
+   public Unicorn(Graphics pen, Point pp, ImageIcon img){
+      super(pen, pp);
+      place = pp;
       setOpaque(false);
       addKeyListener(this);
       
       labelForUser = new JLabel(img);
+      fall = new FallingAction(place);
+      time = new Timer(25, fall);
+      
+      time.start();      
+      repaint();
       
       setFocusable(true);
-
    }
-    public void paintComponent(Graphics pen) { 
    
-      this.add(labelForUser);
-      this.setLocation(place);
-      super.paintComponent(pen);
+   //Is called whenever repaint is called.
+    public void paintComponent(Graphics pen) {
+            this.add(labelForUser);
+      if (100 == fall.getY(place)){
+         super.paintComponent(pen);
+      } else if (fall.getY(place) < 300) {
+         place.y = fall.getY(place);
+         super.paintComponent(pen);  
+      }
+
     }
     
+    //Is called in the keyListener, just changes the y coordinate 
+    //and calls paintComponent.
     public void jump() {
-      place.y -= 50;
+      place.y -= 10;
       repaint();
    }
    
-   public void fall(){
-      place.y += 10;
-      repaint();
-   }
-   
-   public void keyPressed(KeyEvent e){
+      
+   //is called whenever a key is pressed, passing a KeyEvent as a parameter.
+   //if the keyevent is the up key, it calls jump.
+   public void keyPressed(KeyEvent e) {
       int k = e.getKeyCode();
       if(k == KeyEvent.VK_UP){
          jump();
+         System.out.println("up");
       } 
-      if(k == KeyEvent.VK_DOWN){
-         fall();
-      }
    }
    
-      
+   //Not used in our program, but necessary to using keylisteners. 
    public void keyTyped(KeyEvent e){
-      System.out.println("typed");
+      //System.out.println("typed");
    }
    
+   //ditto.
    public void keyReleased(KeyEvent e){
       
-      
-
    }
   
 }
