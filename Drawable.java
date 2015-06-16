38300307406aeac5229a3ff100c08098d4cd9f05
MonicaKing <msmonicaking@gmail.com>
2015-06-16 10:10:21 -0700

    Because of the changes in Scrolling and Drawable, the constructors needed to be changed.

--- Drawable.java
+++ Drawable.java
@@ -1,28 +1,25 @@
+//The Drawable Class is used in every object that will be drawn 
+//on the JFrame, making it a parent (or a grandparent) to every
+//class but Main. 
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Drawable extends JPanel implements ActionListener {
-
+public class Drawable extends JPanel {
    Point place;
    Graphics pen;
-   Timer time;
    
+   //Constructs a new Drawable Object, which is a JPanel. 
+   //Saves the Graphics and the Point.
    public Drawable(Graphics g, Point p) {
       pen = g;
       this.place = p;
-      Timer time = new Timer(50, this);
-      time.start();
    }
    
-    public void actionPerformed(ActionEvent event) {
-      place.y += 5;
-
-      repaint();
-   }
-   
+   //is called every time repaint is called or a listener 
+   //goes off. Sets the location and paints the object.
    public void paintComponent(Graphics pen) {
-   
       setLocation(place);
       super.paintComponent(pen);
    } 
--- Main.java
+++ Main.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.Timer.*;
 
+//The object "Main" creates a JFrame, which it extends, and adds a Unicorn and a Platform. 
+//Very little action is in this object, because the action is stored in the individual 
+//object classes.
 public class Main extends JFrame {
    
+   //The constructor for Main, which is a JFrame.
    Main (String nameOfFrame) {
       super(nameOfFrame);
       setSize(711, 400);
@@ -13,21 +17,22 @@ public class Main extends JFrame {
       setLayout(new BorderLayout());
 	   setContentPane(new JLabel(new ImageIcon("backsample2.jpg")));
       
-      add(new Unicorn(getGraphics(), new Point(-50, 159), new ImageIcon("icon_rainbowdash.png")));
-      add(new Platform(getGraphics(), new Point(-100, 100), new ImageIcon("icon_fluttershy.png")));
-      add(new Platform(getGraphics(), new Point(-50, 200), new ImageIcon("icon_fluttershy.png")));
+      //adds all the drawables and ect. 
+      add(new Unicorn(getGraphics(), new Point(this.getWidth() / -2, 0), new ImageIcon("icon_rainbowdash.png")));
+      add(new Platform(getGraphics(), new Point(50, 100)));
       
       addComponentsToPane(this.getContentPane());
         
       setVisible(true); 
    }
    
+   //when the components are added, it sets the layout of them to a boxlayout.
    public static void addComponentsToPane(Container pane) {
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        
+      pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
    }
    
+   //The main- where a new Main is constructed and named.tre
    public static void main(String[] args) {
-      Main main = new Main("The Frame in Main");
+      Main main = new Main("Friendship is Magic");
    }
 }
