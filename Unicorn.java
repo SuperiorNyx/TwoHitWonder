7a53bc80abd0c9640fe86dedf727a3f0de03f2fa
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2015-06-16 18:06:39 -0700

    Update THW part 3
    Updates Unicorn to its most current form. Elements such as the timer and newly implemented ActionListener replace the FallingAction class

--- Unicorn.java
+++ Unicorn.java
@@ -1,29 +1,95 @@
-//The Unicorn object is also a keylistener, responding to the up key.
-
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color.*;
 
-public class Unicorn extends Drawable implements KeyListener {
+public class Unicorn extends Drawable implements KeyListener, ActionListener{
+
    JLabel labelForUser;
-   FallingAction fall;
    Timer time;
-      
-   //Constructs a Unicorn, passing the Frame's Graphics, the point it is at originally,
-   //and the imageicon that is to be displayed representing the unicorn.
-   public Unicorn(Graphics pen, Point pp, ImageIcon img){
-      super(pen, pp);
-      place = pp;
+   
+   /*
+    * Constructor for a Unicorn object. This object has a Timer that repeatedly makes
+    * it fall, ensuring that jump() works. It adds a KeyListener and ActionListener that enables the user
+    * to control the Unicorn.
+   */
+   public Unicorn(Graphics pen, Point place, ImageIcon img){
+      super(pen, place);
       setOpaque(false);
       addKeyListener(this);
       
       labelForUser = new JLabel(img);
-      fall = new FallingAction(place);
-      time = new Timer(25, fall);
+      time = new Timer(30, this);
+      time.start();
       
-      time.start();      
+      setFocusable(true);
+
+   }
+   
+   //This makes the Unicorn automatically fall so that jump() is functional.
+   public void actionPerformed(ActionEvent event) {
+      place.y += 3;
+
       repaint();
+   }
+   
+   //draws the Unicorn onto the JFrame or JPanel
+   public void paintComponent(Graphics pen) { 
+   
+      this.add(labelForUser);
+      this.setLocation(place);
+      super.paintComponent(pen);
+   }
+   
+   //Moves the Unicorn up 50 pixels, the automatic falling ensures that
+   //jump() doesn't result in the Unicorn staying there 
+   public void jump() {
+      place.y -= 50;
+      repaint();
+   }
+   
+   //This would be used if we added powerups.
+   //It moves the Unicorn right 25 pixels
+   public void dash(){
+      place.x += 25;
+      repaint();
+   }
+   
+   //returns Point place for use outside of this class.
+   public Point getPoint(){
+      return place;
+   }
+   
+   //returns Timer time for use outside of this class.
+   public Timer getTime(){
+      return time;
+   }
+   
+   /*This is part of the KeyListener.
+    *If a certain key - each key emits a key code -
+    *is pressed, then the Unicorn will do an action.
+   */
+   public void keyPressed(KeyEvent e){
+      int k = e.getKeyCode();
+      if(k == KeyEvent.VK_UP){
+         jump();
+      } 
+      if(k == KeyEvent.VK_RIGHT){
+         dash();
+      }   
+   }
+   
+   //Override for KeyListener   
+   public void keyTyped(KeyEvent e){
+      System.out.println("typed");
+   }
+   
+   //Override for KeyListener
+   public void keyReleased(KeyEvent e){   
+
+   }
+  
+}
       
       setFocusable(true);
    }
